248dc039a0bfe97ef84741384be1ec2b2556f95a
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-11-26 20:28:52 +0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (23.39%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // Let's start with 2 pointers approach
+        int p = m - 1; // pointing to last element from the first array
+        int q = n - 1; // pointing to last element from the second array
+        int r = m + n - 1; // pointing to the position of the result (initially on last)
+
+        // Try to iterate from the last element
+        // Get the bigger one to be placed on the last position of the 1st array
+        while (p >= 0 && q >= 0) {
+            if (nums1[p] >= nums2[q]) {
+                nums1[r] = nums1[p]; // placing the biggest from nums1 to the last
+                p--;
+            } else {
+                nums1[r] = nums2[q];
+                q--;
+            }
+            r--;
+        }
+
+        while (q >= 0) {
+            nums1[r] = nums2[q];
+            q--;
+            r--;
+        }
+
+        while (p >= 0) {
+            nums1[r] = nums1[p];
+            p--;
+            r--;
+        }
+    }
+};
