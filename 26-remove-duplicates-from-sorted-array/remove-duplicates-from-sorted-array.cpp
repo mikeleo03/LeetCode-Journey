b26e8eaa2c009b42ba2564b607dc57b425188098
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-11-26 22:15:52 +0700

    Time: 0 ms (100.00%) | Memory: 22.6 MB (5.92%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
-
-        int v = nums[0];
         int k = 1;
+        int v = nums[0];
         for (int i = 1; i < nums.size(); i++) {
-            // Kalo nilainya beda, ganti nums nya
             if (nums[i] != v) {
                 nums[k] = nums[i];
                 v = nums[k];
                 k++;
             }
         }
-
         return k;
     }
 };
