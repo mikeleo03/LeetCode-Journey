c681dec7949ca1df5ea360cea30333a052af04cb
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-11-27 19:12:41 +0700

    Time: 2 ms (37.04%) | Memory: 9.8 MB (5.05%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,23 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int front = 0;
+        int back = s.length() - 1;
+        bool isPal = true;
+
+        while (front <= back) {
+            if (!isalnum(s[front])) {front++; continue;}
+            if (!isalnum(s[back])) {back--; continue;}
+            if (tolower(s[front]) != tolower(s[back])) {
+                return false;
+            } else {front++; back--;}
+        }
+
+        return isPal;
+    }
+};
