acd05b25b8577ac3bdba0bd6948660d2cb3bc01d
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-11-27 19:16:02 +0700

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -13,9 +13,12 @@ public:
         while (front <= back) {
             if (!isalnum(s[front])) {front++; continue;}
             if (!isalnum(s[back])) {back--; continue;}
+            
             if (tolower(s[front]) != tolower(s[back])) {
                 return false;
-            } else {front++; back--;}
+            } else {
+                front++; back--;
+            };
         }
 
         return isPal;
