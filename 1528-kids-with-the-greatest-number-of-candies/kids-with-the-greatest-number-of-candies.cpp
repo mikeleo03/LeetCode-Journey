66730787e1a5cf992df15cbdaf47ff021f431319
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-11-30 01:47:33 +0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (26.46%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> result;
+
+        // satu kali iterate max nya berapa
+        int max = candies[0];
+        for (int i = 1; i < candies.size(); i++) {
+            if (candies[i] > max) max = candies[i];
+        }
+
+        // iterate back to get the bools
+        for (int i = 0; i < candies.size(); i++) {
+            if (candies[i] + extraCandies >= max) {
+                // max = candies[i] + extraCandies;
+                result.push_back(true);
+            } else {
+                result.push_back(false);
+            }
+        }
+
+        return result;
+    }
+};
