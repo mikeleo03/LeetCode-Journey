0eef963259696801ea51e8d55675bf1e9abff81c
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-01 00:03:10 +0700

    Time: 8 ms (49.07%) | Memory: 13.3 MB (5.06%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -5,19 +5,19 @@ public:
     }
 
     int maxVowels(string s, int k) {
-        int maxVows = INT_MIN; int vows = 0;
+        int mv = INT_MIN; int v = 0;
         int i = 0; int j = 0;
 
         while (j < s.length()) {
-            if (isVowel(s[j])) vows++;
+            if (isVowel(s[j])) v++;
             if (j - i + 1 < k) { j++; } 
             else {
-                maxVows = max(maxVows, vows);
-                if (isVowel(s[i])) vows--;
+                mv = max(mv, v);
+                if (isVowel(s[i])) v--;
                 i++; j++;
             }
         }
 
-        return max(maxVows, vows);
+        return max(mv, v);
     }
 };
