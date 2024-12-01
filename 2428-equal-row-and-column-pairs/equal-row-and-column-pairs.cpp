8b8717f7f5f8667ea22c3b4fae6d3796c186e842
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-01 17:17:29 +0700

    Time: 13 ms (74.00%) | Memory: 32.4 MB (71.24%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         map<vector<int>, int> order;
-        map<vector<int>, int> order2;
         int count = 0;
 
         // traverse row and column to check
+        for (int i = 0; i < grid.size(); i++) {
+            order[grid[i]]++;
+        }
+
         for (int i = 0; i < grid.size(); i++) {
             vector<int> arr(grid.size());
             for (int j = 0; j < grid.size(); j++) {
                 arr[j] = grid[j][i];
             }
-            order[grid[i]]++; order2[arr]++;
-        }
-
-        for (auto it : order) {
-            if (order2.find(it.first) != order2.end()) {
-                count += (it.second * order2[it.first]);
-            }
+            count += order[arr];
         }
 
         return count;
