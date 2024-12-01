42f610ab2e7b7f96303a670d3498e4cb8d6ce41d
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-01 09:51:07 +0700

    Time: 20 ms (71.70%) | Memory: 36.6 MB (61.68%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> map1;
-        unordered_map<int, int> map2;
-
-        for (int num : nums1) {
-            if (map1.find(num) != map1.end()) {
-                map1[num]++;
-            } else {
-                map1[num] = 1;
-            }
-        }
+        unordered_set<int> map1(nums1.begin(), nums1.end());
+        unordered_set<int> map2(nums2.begin(), nums2.end());
 
         for (int num : nums2) {
-            if (map2.find(num) == map2.end()) {
-                if (map1.find(num) != map1.end()) {
-                    map1[num] = 0;
-                } else {
-                    map2[num] = 1;
-                }
-            } else {
-                map2[num]++;
+            if (map1.find(num) != map1.end()) {
+                map1.erase(num);
+                map2.erase(num);
             }
         }
 
-        vector<vector<int>> result(2);
-        for (auto it = map1.begin(); it != map1.end(); it++) {
-            if (it->second > 0) result[0].push_back(it->first);
-        }
-
-        for (auto it = map2.begin(); it != map2.end(); it++) {
-            if (it->second > 0) result[1].push_back(it->first);
-        }
-
-        return result;
+        return {vector<int>(map1.begin(), map1.end()), vector<int>(map2.begin(), map2.end())};
     }
 };
