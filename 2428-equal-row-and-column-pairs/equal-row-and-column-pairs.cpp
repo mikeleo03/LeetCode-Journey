29619cc9c275d99511d8f7c499253946c1c8bd3a
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-01 17:03:09 +0700

    Time: 15 ms (69.94%) | Memory: 33.6 MB (65.68%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -5,12 +5,7 @@ public:
         map<vector<int>, int> order2;
         int count = 0;
 
-        // traverse row
-        for (int i = 0; i < grid.size(); i++) {
-            order[grid[i]]++;
-        }
-
-        // traverse column to check
+        // traverse row and column to check
         for (int i = 0; i < grid.size(); i++) {
             vector<int> arr(grid.size());
             for (int j = 0; j < grid.size(); j++) {
@@ -18,34 +13,15 @@ public:
             }
 
             // check is it exist on set
+            order[grid[i]]++;
             order2[arr]++;
         }
-
-        /* for (const auto& pair : order) {
-            cout << "[";
-            for (int i = 0; i < pair.first.size(); i++) {
-                cout << pair.first[i] << ", ";
-            }
-            cout << "] ";
-            cout << pair.second << endl;
-        } */
-
-        /* for (const auto& pair : order2) {
-            cout << "[";
-            for (int i = 0; i < pair.first.size(); i++) {
-                cout << pair.first[i] << ", ";
-            }
-            cout << "] ";
-            cout << pair.second << endl;
-        } */
 
         for (auto it : order) {
             if (order2.find(it.first) != order2.end()) {
                 count += (it.second * order2[it.first]);
             }
         }
-
-        // cout << order.size() << " " << grid.size() * 2 << " " << count << endl;
 
         return count;
     }
