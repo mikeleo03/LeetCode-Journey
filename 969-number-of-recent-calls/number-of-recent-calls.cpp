98d614c9bf75aa0931d24d47e4141f75f6c118c1
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-03 13:38:26 +0700

    Time: 8 ms (95.21%) | Memory: 64.2 MB (8.16%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.cpp
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -9,10 +9,9 @@ public:
         this->counter++; q.push(t);
         
         while (q.front() < t - 3000) {
-            q.pop();
-            this->counter--;
+            q.pop(); this->counter--;
         }
-        
+
         return this->counter;
     }
 };
