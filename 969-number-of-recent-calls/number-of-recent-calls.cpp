893fbae9f939a7e84b14933562f50abe80e21906
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-03 13:39:24 +0700

    Time: 13 ms (80.52%) | Memory: 64.3 MB (6.11%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.cpp
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -1,23 +1,18 @@
 class RecentCounter {
 public:
-    queue<int> q;
     int counter;
-
-    RecentCounter() : counter(0) {}
+    std::queue<int> pings;
+    RecentCounter() {
+        counter = 0;
+    }
     
     int ping(int t) {
-        this->counter++; q.push(t);
-        
-        while (q.front() < t - 3000) {
-            q.pop(); this->counter--;
+        this->counter++;
+        pings.push(t);
+        while (t - 3000 > pings.front()) {
+            pings.pop();
+            this->counter--;
         }
-
         return this->counter;
     }
 };
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter* obj = new RecentCounter();
- * int param_1 = obj->ping(t);
- */
