509c7ca0ff13609cd031b0d5af0286832379f960
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-03 09:27:29 +0700

    Time: 6 ms (9.62%) | Memory: 22.3 MB (5.89%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> s;
+
+        for (int ast : asteroids) {
+            if (ast > 0) {
+                s.push(ast);
+            } else {
+                while (!s.empty() && s.top() > 0 && s.top() < -ast) {
+                    s.pop();
+                }
+
+                if (s.empty() || s.top() < 0) {
+                    s.push(ast);
+                }
+
+                if (!s.empty() && s.top() == -ast) {
+                    s.pop();
+                }
+            }
+        }
+
+        vector<int> res(s.size());
+        int i = s.size() - 1;
+
+        while (!s.empty()) {
+            res[i--] = s.top();
+            s.pop();
+        }
+        return res;
+    }
+};
