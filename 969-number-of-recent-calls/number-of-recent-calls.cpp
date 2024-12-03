a43e5c2ff7b8845c3fb39acb43ac180e33ec9246
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-03 13:43:22 +0700

    Time: 29 ms (18.22%) | Memory: 64.2 MB (6.11%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.cpp
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -2,7 +2,6 @@ class RecentCounter {
 public:
     queue<int> q;
     int counter;
-
     RecentCounter() : counter(0) {}
     
     int ping(int t) {
@@ -10,6 +9,7 @@ public:
         while (q.front() < t - 3000) {
             q.pop(); this->counter--;
         }
+
         return this->counter;
     }
 };
