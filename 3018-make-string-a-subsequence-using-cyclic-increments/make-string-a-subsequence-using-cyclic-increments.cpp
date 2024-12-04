dc0f4bc0b293ccae665df67b90efa4eb36c46081
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-04 09:49:19 +0700

    Time: 4 ms (51.93%) | Memory: 17.2 MB (7.53%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    char cyclicIncrement(char c) {
-        return (c - 'a' + 1) % 26 + 'a';
-    }
-
-    bool canMakeSubsequence(string str1, string str2) {
-        int p1 = 0; int p2 = 0;
-
-        while (p1 < str1.length()) {
-            if (str1[p1] == str2[p2] || cyclicIncrement(str1[p1]) == str2[p2]) {
-                p1++; p2++;
-            } else {
-                p1++;
+    bool canMakeSubsequence(string source, string target) {
+        int targetIdx = 0;
+        int targetLen = target.length();
+        for (char currChar : source) {
+            if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1) {
+                targetIdx++;
             }
         }
-        return (p2 == str2.length());
+        return targetIdx == targetLen;
     }
 };
