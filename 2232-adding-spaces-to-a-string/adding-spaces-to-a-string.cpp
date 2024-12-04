bed5b49ed4ab95a014acc15791a79122649ed6eb
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-04 09:36:09 +0700

    Time: 3 ms (100.00%) | Memory: 80.7 MB (91.14%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -4,12 +4,10 @@ public:
         const int m = spaces.size(), n = s.size();
         string t(n + m, ' ');
         
-        int j = 0; // Pointer for spaces
+        int j = 0; // space pointer
         for (int i = 0; i < n; i++) {
-            if (j < m && i == spaces[j]) 
-                j++;      // Move to the next space index
-            
-            t[i+j]=s[i]; // Add the character from the original string
+            if (j < m && i == spaces[j]) j++;      // Move to the next space index
+            t[i + j] = s[i]; // Add the character from the original string
         }
         return t;
     }
