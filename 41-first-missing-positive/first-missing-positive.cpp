71641e1038c410dd1523ffc3bbab0d7b103aa06f
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-08 13:22:27 +0700

    Time: 61 ms (21.52%) | Memory: 78.4 MB (15.15%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        nums.erase(remove_if(nums.begin(), nums.end(), [](int n) { return n <= 0; }), nums.end());
-        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        unordered_set<int> numSet(nums.begin(), nums.end());
 
-        int target = 1;
-        for (int n : nums) {
-            if (n == target) {
-                target++;
-            } else if (n > target) {
-                return target;
+        for (int i = 1; i <= n + 1; i++) {
+            if (numSet.find(i) == numSet.end()) {
+                return i; 
             }
         }
-        
-        return target;
+
+        return -1;
     }
 };
