7a79bbaff1bed635d6b9c8e62f2827fb7c240155
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-08 13:26:01 +0700

    Time: 0 ms (100.00%) | Memory: 54.9 MB (40.49%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        nums.erase(remove_if(nums.begin(), nums.end(), [](int n) { return n <= 0; }), nums.end());
-        sort(nums.begin(), nums.end());
-
-        int target = 1;
-        for (int n : nums) {
-            if (n == target) {
-                target++;
-            } else if (n > target) {
-                return target;
+        if (nums.size() == 1) {
+            if (nums[0] <= 0) {
+                return 1;
+            } else if(nums[0] == 1) {
+                return 2;
             }
+            return 1;
         }
         
-        return target;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (abs(nums[i]) > n) {
+                continue;
+            } else {
+                if (nums[abs(nums[i]) - 1] < 0) {
+                    continue;
+                } else {
+                    nums[abs(nums[i]) - 1] *= -1;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                return i + 1;
+            }
+        }
+
+        return n+1;
     }
 };
