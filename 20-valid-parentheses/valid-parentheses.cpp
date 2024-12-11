afc51679020156a7674e82bbcbd356c8949d5859
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-11 19:56:33 +0700

    Time: 2 ms (14.56%) | Memory: 8.8 MB (17.13%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.length() % 2 == 1) return false;
+
+        stack<char> q;
+
+        for (char c : s) {
+            if (!q.empty()) {
+                if (c == ')' && q.top() == '(') {
+                    q.pop();
+                } else if (c == ']' && q.top() == '[') {
+                    q.pop();
+                } else if (c == '}' && q.top() == '{') {
+                    q.pop();
+                } else {
+                    q.push(c);
+                }
+            } else {
+                q.push(c);
+            }
+        }
+
+        return q.empty();
+    }
+};
