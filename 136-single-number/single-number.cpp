ea57bd7e27fdf7c0210518a2a3bfca0ec3d2b302
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-14 23:14:56 +0700

    Time: 0 ms (100.00%) | Memory: 20.6 MB (31.20%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,20 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> map;
-
-        for (int num : nums) {
-            if (map.find(num) != map.end()) {
-                map.erase(num);
-            } else {
-                map.insert(num);
-            }
-        }
-
-        for (auto it = map.begin(); it != map.end(); it++) {
-            return *it;
-        }
-
-        return -1;
+        int ans = 0;
+	    for (auto num : nums) ans ^= num;
+	    return ans;
     }
 };
