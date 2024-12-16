26b2700f1d629554dbc291f7990feddcce781601
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-16 23:56:27 +0700

    Time: 53 ms (20.77%) | Memory: 75.7 MB (9.47%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> set;
-        for (int num : nums) {
-           if (set.find(num) != set.end()) {
-                return true;
-            }
-            set.insert(num);
-        }
-
-        return false;
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        return numSet.size() < nums.size(); 
     }
 };
