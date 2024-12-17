c9ee36f81f63546dc95e3ab2fcf428d941af4e15
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2024-12-17 23:31:00 +0700

    Time: 0 ms (100.00%) | Memory: 8.6 MB (16.11%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double binaryExp(double x, long n) {
+        if (n == 0) return 1;
+       
+        if (n < 0) {
+            return 1.0 / binaryExp(x, -n);
+        }
+       
+        if (n % 2 == 1) {
+            return x * binaryExp(x * x, (n - 1) / 2);
+        } else {
+            return binaryExp(x * x, n / 2);
+        }
+    }
+
+    double myPow(double x, int n) {
+        return binaryExp(x, static_cast<long>(n));
+    }
+};
