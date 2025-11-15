5d0d0b98f824d8945656c9bdf116d61426a34397
Michael Leon Putra Widhi <91791710+mikeleo03@users.noreply.github.com>
2025-11-15 23:30:21 +0700

    Time: 0 ms (100.00%) | Memory: 32 MB (96.93%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        // awalnya front back nya 0
-        // init sum = 0
-        // while front blom sampe ujung dan back blom sampe ujung
-        // kalo pos front sama pos backnya sama, cek nilainya sama kek target ga, klo sama return 1 (udh pasti paling kecil)
-        // kalo beda, mulai extend window, update state sum
-        // 
-        // cek state sum
-        // a. kalo lebih besar ato sama, update state size, trus shrink window
-
         // State
         int front = 0; int back = 0;
         int sum = nums[front]; 
@@ -28,9 +19,7 @@ public:
                 sum -= nums[back];
                 back++;
                 shrinked = true;
-            }
-
-            cout << back << " " << front << " " << minSize << endl; 
+            } 
         }
 
         // Return
